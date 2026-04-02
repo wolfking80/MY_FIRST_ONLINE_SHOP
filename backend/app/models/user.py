7cb0782480cbbc8ba5b89f67c56812a3bf0548ea
wolfking80 <wolfking05101980@gmail.com>
2026-04-02 07:44:57 +0700

    feat(users): add the column "hashed_password" to User model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,6 +6,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password=Column(String, nullable=False)
     first_name = Column(String)
     last_name = Column(String)
     phone = Column(String, unique=True)
